a5f78c21de77c131a3884a2e878cee44a418be79
Homer White <homerhanumat@gmail.com>
2018-11-09 09:44:34 -0500

    add basic3

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 var http = require("http");
 var path = require("path");
 var express = require("express");
+const bodyParser = require("body-parser");
+const session = require("cookie-session");
+const fs = require("fs");
+
+const users = JSON.parse(fs.readFileSync("./data/users.json"));
 
 var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  name: "session",
+  secret: "hey there",
+  //keys: process.env.KEY,
+  maxAge: 60000,
+  secure: true,
+  httpOnly: true
+
+}))
+
+// array of secure locations:
+secureURLs = ["/confidential"];
+// this function will be applied to every request:
+function checkAuth (req, res, next) {
+	if (secureURLs.includes(req.url) && (!req.session || !req.session.authenticated)) {
+		res.render('secure-failure');
+		return;
+	}
+	next();
+}
 
 app.get("/", function(req, res) {
   res.render("index");
@@ -25,11 +51,41 @@ app.get("/thanks", function(req, res) {
   res.render("thanks");
 });
 
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.post('/login', function (req, res) {
+  let authenticated = false;
+  let user;
+  users.forEach(elem => {
+    if (elem.username === req.body.username && elem.password === req.body.password) {
+      authenticated = true;
+      user = elem;
+      return;
+    }
+  });
+  if (authenticated) {
+    req.session.authenticated = true;
+    req.session.username = user.username;
+    req.session.role = user.role;
+    res.render("login-success", { 
+      user: user.username
+    });
+  } else {
+    res.render('login-failure');
+  }
+});
+
+app.get("/confidential", function(req, res) {
+  res.render("confidential");
+});
+
 
 app.use(function(req, res) {
   res.status(404).render("404");
 });
 
 http.createServer(app).listen(3000, function() {
-  console.log("Basic app (with pure css) started.");
+  console.log("Basic app (with login) started.");
 });
